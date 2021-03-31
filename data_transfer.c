ee79fc19575206df648e0dcb3d3f7ce9f5a0aa71
ppitu <piotr.chmura.1998@o2.pl>
2021-03-31 22:52:17 +0200

    add

--- data_transfer.c
+++ data_transfer.c
@@ -1,8 +1,43 @@
-#include 
+#include <string.h>
 
 
 #include "data_transfer.h"
 #include "global_define.h"
+
+void
+divide_string	(char		*str,
+		char		**result_array,
+		int		*array_size)
+{
+	int		str_size;
+	int		i;
+	int		size_of_last_portion;
+
+	str_size = strlen(str);
+
+	if((str_size % BUFFER_SIZE) == 0)
+	{
+		*array_size = str_size / BUFFER_SIZE;
+	} else 
+	{
+		*array_size = str_size / BUFFER_SIZE + 1;
+	}
+
+	result_array = malloc(*array_size * sizeof(char *));
+
+	size_of_last_portion = str_size % BUFFER_SIZE;
+
+	for(i = 0; i < *array_size; i++)
+	{
+		*(result_array + i) = malloc(BUFFER_SIZE * sizeof(char));
+		
+		if((*array_size - 1) == i)
+		{
+			
+		}
+	}
+}
+
 
 void
 send_data	(int		sockfd,
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	char *name = "Hello Peter";
+
+	printf("%c\n", name[0]);
+}
